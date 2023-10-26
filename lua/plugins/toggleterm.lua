e74c35a1522954fca61d28b5c55ccab238cfee86
Ilya Rusyanov <rusyanov@monitorsoft.ru>
2023-10-26 10:59:11 +0300

    toggleterm is vertical

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,13 @@
 require("toggleterm").setup{
+  size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+  end,
   open_mapping = [[<C-\>]],
+  direction = 'vertical',
 }
 
 function _G.set_terminal_keymaps()
