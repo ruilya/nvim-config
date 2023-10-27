c52af1cb76f15ec5b0e796bc8a5f2d9c00c267ad
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2023-10-27 09:48:48 +0300

    fix indent

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -24,26 +24,28 @@ require("lazy").setup{
                 ensure_installed = { "c", "lua", "rust" },
                 highlight = { enable = true, }
             }
-        end },
+        end
+    },
     { "nvim-telescope/telescope.nvim",
        tag = "0.1.2",
-    dependencies = "nvim-lua/plenary.nvim",
-    keys = {
-        { "<C-t>", "<CMD>Telescope<CR>", mode = { "n", "i", "v" } },
-        { "<leader>ff", "<CMD>Telescope find_files<CR>", mode = { "n", "i", "v" } },
-        { "<leader>fg", "<CMD>Telescope live_grep<CR>", mode = { "n", "i", "v" } },
-        { "<leader>fb", "<CMD>Telescope buffers<CR>", mode = { "n", "i", "v" } },
-        { "<leader>fh", "<CMD>Telescope help_tags<CR>", mode = { "n", "i", "v" } },
-        --{ "<C-c>", "<CMD>Telescope commands<CR>", mode = { "n", "i", "v" } },
-        { "<C-k>", "<CMD>Telescope keymaps<CR>", mode = { "n", "i", "v" } },
-        { "<C-s>", "<CMD>Telescope grep_string<CR>", mode = { "n", "i", "v" } },
+       dependencies = "nvim-lua/plenary.nvim",
+       keys = {
+           { "<C-t>", "<CMD>Telescope<CR>", mode = { "n", "i", "v" } },
+           { "<leader>ff", "<CMD>Telescope find_files<CR>", mode = { "n", "i", "v" } },
+           { "<leader>fg", "<CMD>Telescope live_grep<CR>", mode = { "n", "i", "v" } },
+           { "<leader>fb", "<CMD>Telescope buffers<CR>", mode = { "n", "i", "v" } },
+           { "<leader>fh", "<CMD>Telescope help_tags<CR>", mode = { "n", "i", "v" } },
+           --{ "<C-c>", "<CMD>Telescope commands<CR>", mode = { "n", "i", "v" } },
+           { "<C-k>", "<CMD>Telescope keymaps<CR>", mode = { "n", "i", "v" } },
+           { "<C-s>", "<CMD>Telescope grep_string<CR>", mode = { "n", "i", "v" } },
+       },
+       config = true
     },
-    config = true },
     {
-    "iamcco/markdown-preview.nvim",
-    config = function()
-      vim.fn["mkdp#util#install"]()
-    end,
+        "iamcco/markdown-preview.nvim",
+        config = function()
+          vim.fn["mkdp#util#install"]()
+        end,
     },
     { "fatih/vim-go", },
     { "ishan9299/nvim-solarized-lua" },
