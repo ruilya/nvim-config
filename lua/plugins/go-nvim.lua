5622c0242534a95aabf2f3df98e0d00d2fb237bf
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2024-03-15 10:53:10 +0300

    go organize imports with lsp

--- lua/plugins/go-nvim.lua
+++ lua/plugins/go-nvim.lua
@@ -1,12 +1,2 @@
--- Run gofmt + goimport on save
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
-})
-
 vim.keymap.set('n', '<leader>b', ':GoBuild %:h<CR>')
 vim.keymap.set('n', '<leader>gt', ':GoTestPkg<CR>')
