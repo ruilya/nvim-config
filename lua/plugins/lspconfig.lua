d855034a78fd6df067d6057bc05d207110fb7b3c
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2024-03-20 14:26:09 +0300

    lsp restart hotkey

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -48,6 +48,7 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>lr', ':LspRestart<CR>')
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
