f89f292db3351a18bd9569e1901b0b5af1407240
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2024-03-27 15:33:06 +0300

    change theme

--- lua/plugins/git-blame-nvim.lua
+++ lua/plugins/git-blame-nvim.lua
@@ -2,5 +2,5 @@ require('gitblame').setup {
      --Note how the `gitblame_` prefix is omitted in `setup`
     enabled = true,
     message_template = '<summary> • <date> • <author>',
-    date_format = '%a %x %H:%M'
+    date_format = '%a %x %H:%M',
 }
