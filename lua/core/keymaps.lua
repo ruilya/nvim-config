5129bd50f3443bd51bf5923823b56a2588b3b96a
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2024-12-25 13:50:06 +0300

    toggle diagnostic command

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,3 +19,14 @@ vim.keymap.set('n',  '<leader>n', require("nvim-navbuddy").open, {noremap=true})
 vim.keymap.set('n', '<leader>th',
                function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
                { noremap = true, silent = true })
+
+-- https://www.reddit.com/r/neovim/comments/13wcqdr/disable_hintslsperrors_etc/
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
