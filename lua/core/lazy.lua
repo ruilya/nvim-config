ddeda69c65ef0ad520bd667c191cd75249bc53f8
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2025-03-27 18:47:24 +0300

    nvim-cmp init as in official doc

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -107,7 +107,10 @@ require("lazy").setup{
     {'hrsh7th/cmp-buffer'},
     {'hrsh7th/cmp-path'},
     {'hrsh7th/cmp-cmdline'},
-    {'hrsh7th/nvim-cmp'},
+    {
+        'hrsh7th/nvim-cmp',
+        event = { "InsertEnter", "CmdlineEnter" }
+    },
     { 'saadparwaiz1/cmp_luasnip' }, -- Snippets source for nvim-cmp
     { "SmiteshP/nvim-navbuddy" },
     {
