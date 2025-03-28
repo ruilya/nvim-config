d9687330595a1f36da74e389fa5266a05fb7f591
Ilya Rusyanov <ilya.rusyanov@gmail.com>
2025-03-28 17:34:33 +0300

    remove binding for diagnostic move
    in nvim 0.11 it is already mapped

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -55,9 +55,8 @@ end
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
 vim.keymap.set('n', '<leader>lr', ':LspRestart<CR>')
 
 -- Use LspAttach autocommand to only map the following keys
